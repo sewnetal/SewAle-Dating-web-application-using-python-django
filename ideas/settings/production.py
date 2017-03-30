40c1753710e8160dc5275df5d14b4f6de1cebddd
Gebremedhin Sewnet Alemseged <Sewnet@dyn139-94.eduroam.utu.fi>
2017-03-30 13:59:56 +0300

    new page handler or view

--- ideas/settings/production.py
+++ ideas/settings/production.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'ideas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
